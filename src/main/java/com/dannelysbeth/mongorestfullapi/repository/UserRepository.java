3b6e146a2ad4401178aee08380385dd8e4d409da
Dannelysbeth <danielaf@onet.pl>
2025-01-27 23:32:49 +0100

    corrected README

--- src/main/java/com/dannelysbeth/mongorestfullapi/repository/UserRepository.java
+++ src/main/java/com/dannelysbeth/mongorestfullapi/repository/UserRepository.java
@@ -10,9 +10,5 @@ import java.util.Optional;
 public interface UserRepository
         extends MongoRepository<User, String> {
 
-    boolean existsByEmail(String email);
 
-    Optional<User> getUserByEmail(String email);
-
-    Optional<User> getUserByUsername(String username);
 }
